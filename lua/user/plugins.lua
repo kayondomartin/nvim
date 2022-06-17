4c0b9559d051d5e6375c8b8f9e6aa6125f9d1dbd
Kayondo Martin <kayondomartin45@gmail.com>
2022-06-17 18:42:40 +0900

    changing everything

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,11 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  use {
+    'kosayoda/nvim-lightbulb',
+    requires = 'antoinemadec/FixCursorHold.nvim'
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
